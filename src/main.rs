9af7b1ddf3eb073e368d199674039f33856fd584
Siddharth Parashar <20308880+shanu516516@users.noreply.github.com>
2022-01-14 19:15:55 +0530

    traderorder updated and impl fn insert order added

--- src/main.rs
+++ src/main.rs
@@ -16,5 +16,7 @@ fn main() {
     // .newtraderorderinsert(); //only call this function if docker is running with postgresql
 
     println!("1st: {:#?}", o);
+
+    // {"uuid":"3537d499-9498-41f4-9a12-0f25f4864195","account_id":"account_id","position_type":"SHORT","position_side":1,"order_status":"PENDING","order_type":"MARKET","entryprice":42514.01,"execution_price":0.0,"positionsize":3231277330.05,"leverage":5.0,"initial_margin":15201.0,"available_margin":15201.0,"timestamp":1642167758764,"bankruptcy_price":53142.512500000004,"bankruptcy_value":60804.0,"maintenance_margin":320.43708,"liquidation_price":52863.919643478876}
     println!("2nd: {}", o.serialize());
 }
