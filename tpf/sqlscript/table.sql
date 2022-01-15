39a52cd8a12e9ae1efc935322f9d22e46e3ffb91
Siddharth Parashar <20308880+shanu516516@users.noreply.github.com>
2022-01-15 23:32:08 +0530

    unrealized_pnl added in traderorder table

--- tpf/sqlscript/table.sql
+++ tpf/sqlscript/table.sql
@@ -32,6 +32,7 @@ CREATE TABLE newtraderorder(
   ,bankruptcy_value   NUMERIC NOT NULL
   ,maintenance_margin NUMERIC NOT NULL
   ,liquidation_price  NUMERIC NOT NULL
+  ,unrealized_pnl  NUMERIC NOT NULL
 );
 -- INSERT INTO testtable1(uuid,account_id,position_type,position_side,order_status,order_type,entryprice,execution_price,positionsize,leverage,initial_margin,available_margin,timestamp,bankruptcy_price,bankruptcy_value,maintenance_margin) VALUES ('1d5e4a52-5918-43ee-b8ed-dd2a3d89e34f',N'account_id','SHORT',1,'PENDING','MARKET',42514.01,0,3231277330.05,5,15201,15201,'1642155902808',53142.512500000004,60804,320.43708);
 
