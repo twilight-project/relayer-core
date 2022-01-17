2b6859ee3456f8be93317f633a623d02e973e922
Siddharth Parashar <20308880+shanu516516@users.noreply.github.com>
2022-01-18 01:29:54 +0530

    testing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 mod perpetual;
 use perpetual::interface::{OrderStatus, OrderType, PositionType, TraderOrder};
 extern crate tpf;
-
+mod ordertest;
 extern crate stopwatch;
 use std::{thread, time};
 use stopwatch::Stopwatch;
+use tpf::redislib::redis_db;
 
 fn main() {
     let one_sec = time::Duration::from_millis(1000);
@@ -23,11 +24,26 @@ fn main() {
         0.0,
     );
     // .newtraderorderinsert(); //only call this function if docker is running with postgresql
+    ordertest::generateorder();
+
     println!("Thing took {}ms", sw.elapsed_ms());
 
-    // println!("1st: {:#?}", o);
+    println!("1st: {:#?}", o);
 
     // {"uuid":"3537d499-9498-41f4-9a12-0f25f4864195","account_id":"account_id","position_type":"SHORT","position_side":1,"order_status":"PENDING","order_type":"MARKET","entryprice":42514.01,"execution_price":0.0,"positionsize":3231277330.05,"leverage":5.0,"initial_margin":15201.0,"available_margin":15201.0,"timestamp":1642167758764,"bankruptcy_price":53142.512500000004,"bankruptcy_value":60804.0,"maintenance_margin":320.43708,"liquidation_price":52863.919643478876}
     println!("2nd: {}", o.serialize());
+
     thread::sleep(one_sec);
+
+    println!(
+        "{:#?}",
+        redis_db::zrangelonglimitorderbyexecutionprice(50000.0)
+    );
+    println!(
+        " {:#?}",
+        redis_db::zrangeshortlimitorderbyexecutionprice(39000.0)
+    );
+
+    println!(" {:#?}", redis_db::zrangegetliquidateorderforshort(52300.0));
+    println!(" {:#?}", redis_db::zrangegetliquidateorderforlong(35500.0));
 }
