622b4d479137db88836ad5fed7829daa246c225e
Siddharth Parashar <20308880+shanu516516@users.noreply.github.com>
2022-01-20 02:05:10 +0530

    test fn

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 mod perpetual;
-use perpetual::interface::{OrderStatus, OrderType, PositionType, TraderOrder};
+use perpetual::interface::{
+    getandupdateallordersonfundingcycle, updatefundingrate, OrderStatus, OrderType, PositionType,
+    TraderOrder,
+};
 extern crate tpf;
 mod ordertest;
 extern crate stopwatch;
@@ -14,36 +17,45 @@ fn main() {
 
     let o: TraderOrder = TraderOrder::new(
         "account_id",
-        PositionType::SHORT,
+        PositionType::LONG,
         OrderType::MARKET,
-        5.0,
-        15201.0,
-        15201.0,
+        10.0,
+        1000.0,
+        1000.0,
         OrderStatus::PENDING,
-        42514.01,
+        40000.00,
         0.0,
-    );
-    // .newtraderorderinsert(); //only call this function if docker is running with postgresql
+    )
+    .newtraderorderinsert(); //only call this function if docker is running with postgresql
     ordertest::generateorder();
 
-    println!("Thing took {}ms", sw.elapsed_ms());
+    // println!("Thing took {}ms", sw.elapsed_ms());
+    println!("Thing took {:#?}", sw.elapsed());
 
     println!("1st: {:#?}", o);
 
     // {"uuid":"3537d499-9498-41f4-9a12-0f25f4864195","account_id":"account_id","position_type":"SHORT","position_side":1,"order_status":"PENDING","order_type":"MARKET","entryprice":42514.01,"execution_price":0.0,"positionsize":3231277330.05,"leverage":5.0,"initial_margin":15201.0,"available_margin":15201.0,"timestamp":1642167758764,"bankruptcy_price":53142.512500000004,"bankruptcy_value":60804.0,"maintenance_margin":320.43708,"liquidation_price":52863.919643478876}
     println!("2nd: {}", o.serialize());
 
+    // thread::sleep(one_sec);
+
+    // println!(
+    //     "{:#?}",
+    //     redis_db::zrangelonglimitorderbyexecutionprice(50000.0)
+    // );
+    // println!(
+    //     " {:#?}",
+    //     redis_db::zrangeshortlimitorderbyexecutionprice(39000.0)
+    // );
+
+    // println!(" {:#?}", redis_db::zrangegetliquidateorderforshort(52300.0));
+    // println!(" {:#?}", redis_db::zrangegetliquidateorderforlong(35500.0));
+
+    // updatefundingrate(0.1);
     thread::sleep(one_sec);
-
-    println!(
-        "{:#?}",
-        redis_db::zrangelonglimitorderbyexecutionprice(50000.0)
-    );
-    println!(
-        " {:#?}",
-        redis_db::zrangeshortlimitorderbyexecutionprice(39000.0)
-    );
-
-    println!(" {:#?}", redis_db::zrangegetliquidateorderforshort(52300.0));
-    println!(" {:#?}", redis_db::zrangegetliquidateorderforlong(35500.0));
+    redis_db::set(&"CurrentPrice", &"40400.00");
+    thread::sleep(one_sec);
+    o.calculatepayment();
+    // getandupdateallordersonfundingcycle();
+    thread::sleep(one_sec);
 }
