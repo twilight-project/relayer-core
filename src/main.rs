eec54b5db5c715cf0a83badc673e28ec84dbd5de
Siddharth Parashar <20308880+shanu516516@users.noreply.github.com>
2022-02-03 01:34:19 +0530

    pending order major bug fixed

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,59 @@
 // extern crate stopwatch;
 mod config;
-// mod kafkalib;
-// mod ordertest;
-// mod postgresqllib;
-// mod pricefeederlib;
-// mod redislib;
-// mod relayer;
-// mod utils;
-// use redislib::redis_db;
-// use relayer::*;
-// use std::{thread, time};
-// use stopwatch::Stopwatch;
+mod kafkalib;
+mod ordertest;
+mod postgresqllib;
+mod pricefeederlib;
+mod redislib;
+mod relayer;
+mod utils;
 use config::BUSYSTATUS;
+use redislib::redis_db;
+use relayer::*;
+use std::{thread, time};
+use stopwatch::Stopwatch;
 #[macro_use]
 extern crate lazy_static;
 
-// fn main() {
-//     let one_sec = time::Duration::from_millis(1000);
-
-//     let sw = Stopwatch::start_new();
-
-//     ordertest::initprice();
-//     // loop {
-//     // thread::sleep(one_sec);
-//     // thread::sleep(one_sec);
-//     // thread::sleep(one_sec);
-
-//     // ordertest::generatelendorder();
-//     // }
-//     ordertest::generateorder();
-
-//     // println!("Thing took {}ms", sw.elapsed_ms());
-//     println!("Thing took {:#?}", sw.elapsed());
-
-//     thread::sleep(one_sec);
-
-//     // println!(
-//     //     "{:#?}",
-//     //     redis_db::zrangelonglimitorderbyexecutionprice(50000.0)
-//     // );
-//     // println!(
-//     //     " {:#?}",
-//     //     redis_db::zrangeshortlimitorderbyexecutionprice(39000.0)
-//     // );
-
-//     // println!(" {:#?}", redis_db::zrangegetliquidateorderforshort(52300.0));
-//     // println!(" {:#?}", redis_db::zrangegetliquidateorderforlong(35500.0));
-
-//     // updatefundingrate(0.1);
-//     // redis_db::set(&"CurrentPrice", &"38000.0");
-//     // o.calculatepayment();
-//     // getandupdateallordersonfundingcycle();
-//     thread::sleep(one_sec);
-// }
-
 fn main() {
-    // println!("{}", *BUSYSTATUS.lock().unwrap());
+    let one_sec = time::Duration::from_millis(1000);
+
+    let sw = Stopwatch::start_new();
+
+    ordertest::initprice();
+    // loop {
+    // thread::sleep(one_sec);
+    // thread::sleep(one_sec);
+    // thread::sleep(one_sec);
+
+    // ordertest::generatelendorder();
+    // }
+    ordertest::generateorder();
+
+    // println!("Thing took {}ms", sw.elapsed_ms());
+    println!("Thing took {:#?}", sw.elapsed());
+
+    thread::sleep(one_sec);
+
+    // println!(
+    //     "{:#?}",
+    //     redis_db::zrangelonglimitorderbyexecutionprice(50000.0)
+    // );
+    // println!(
+    //     " {:#?}",
+    //     redis_db::zrangeshortlimitorderbyexecutionprice(39000.0)
+    // );
+
+    // println!(" {:#?}", redis_db::zrangegetliquidateorderforshort(52300.0));
+    // println!(" {:#?}", redis_db::zrangegetliquidateorderforlong(35500.0));
+
+    // updatefundingrate(0.1);
+    // redis_db::set(&"CurrentPrice", &"38000.0");
+    // o.calculatepayment();
+    // getandupdateallordersonfundingcycle();
+    thread::sleep(one_sec);
 }
+
+// fn main() {
+//     // println!("{}", *BUSYSTATUS.lock().unwrap());
+// }
--- src/relayer/pricetickerupdate.rs
+++ src/relayer/pricetickerupdate.rs
@@ -1,9 +1,9 @@
+use crate::config::POSTGRESQL_POOL_CONNECTION;
 use crate::redislib::redis_db;
 use crate::relayer::traderorder::TraderOrder;
 use crate::relayer::types::*;
 use std::thread;
 
-//Pending Limit orders
 pub fn check_pending_limit_order_on_price_ticker_update() {
     let current_price = redis_db::get("CurrentPrice").parse::<f64>().unwrap();
     let orderid_list_short = redis_db::zrangegetpendinglimitorderforshort(current_price);
@@ -19,62 +19,104 @@ pub fn check_pending_limit_order_on_price_ticker_update() {
         update_limit_pendingorder(ordertx);
     }
 }
-
+// error issues
 pub fn update_limit_pendingorder(ordertx: TraderOrder) {
     let rt = ordertx.clone();
+
+    match rt.position_type {
+        PositionType::LONG => {
+            redis_db::zdel(
+                &"TraderOrder_LimitOrder_Pending_FOR_Long",
+                &rt.uuid.to_string(),
+            );
+        }
+        PositionType::SHORT => {
+            redis_db::zdel(
+                &"TraderOrder_LimitOrder_Pending_FOR_Short",
+                &rt.uuid.to_string(),
+            );
+        }
+    }
+
+    let mut pending_order: TraderOrder = TraderOrder::new(
+        &rt.account_id,
+        rt.position_type,
+        rt.order_type,
+        rt.leverage,
+        rt.initial_margin,
+        rt.available_margin,
+        OrderStatus::PENDING,
+        rt.entryprice,
+        rt.execution_price,
+    );
+    pending_order.uuid = rt.uuid;
+    pending_order.newtraderorderinsert();
+
+    let query = format!(
+        "UPDATE public.pendinglimittraderorder
+    SET order_status='{:#?}'
+    WHERE uuid='{}';",
+        &rt.order_status, &rt.uuid
+    );
     thread::spawn(move || {
-        // trader order saved in redis, orderid as key
-        redis_db::set(&rt.uuid.to_string(), &rt.serialize());
+        let mut client = POSTGRESQL_POOL_CONNECTION.get().unwrap();
 
-        redis_db::zadd(
-            &"TraderOrder",
-            &rt.uuid.to_string(),      //value
-            &rt.timestamp.to_string(), //score
-        );
-
-        // update pool size when new order get inserted
-
-        match rt.position_type {
-            PositionType::LONG => {
-                redis_db::incrbyfloat(&"TotalLongPositionSize", &rt.positionsize.to_string());
-                // trader order set by liquidation_price for long
-                redis_db::zadd(
-                    &"TraderOrderbyLiquidationPriceFORLong",
-                    &rt.uuid.to_string(),
-                    &rt.liquidation_price.to_string(),
-                );
-            }
-            PositionType::SHORT => {
-                redis_db::incrbyfloat(&"TotalShortPositionSize", &rt.positionsize.to_string());
-                // trader order set by liquidation_price for short
-                redis_db::zadd(
-                    &"TraderOrderbyLiquidationPriceFORShort",
-                    &rt.uuid.to_string(),
-                    &rt.liquidation_price.to_string(),
-                );
-            }
-        }
-
-        redis_db::incrbyfloat(&"TotalPoolPositionSize", &rt.positionsize.to_string());
-
-        match rt.order_type {
-            OrderType::LIMIT => match rt.position_type {
-                PositionType::LONG => {
-                    redis_db::zadd(
-                        &"TraderOrderbyLONGLimit",
-                        &rt.uuid.to_string(),
-                        &rt.execution_price.to_string(),
-                    );
-                }
-                PositionType::SHORT => {
-                    redis_db::zadd(
-                        &"TraderOrderbySHORTLimit",
-                        &rt.uuid.to_string(),
-                        &rt.execution_price.to_string(),
-                    );
-                }
-            },
-            _ => {}
-        }
+        client.execute(&query, &[]).unwrap();
     });
+
+    // thread::spawn(move || {
+    //     // trader order saved in redis, orderid as key
+    //     redis_db::set(&rt.uuid.to_string(), &rt.serialize());
+
+    //     redis_db::zadd(
+    //         &"TraderOrder",
+    //         &rt.uuid.to_string(),      //value
+    //         &rt.timestamp.to_string(), //score
+    //     );
+
+    //     // update pool size when new order get inserted
+
+    //     match rt.position_type {
+    //         PositionType::LONG => {
+    //             redis_db::incrbyfloat(&"TotalLongPositionSize", &rt.positionsize.to_string());
+    //             // trader order set by liquidation_price for long
+    //             redis_db::zadd(
+    //                 &"TraderOrderbyLiquidationPriceFORLong",
+    //                 &rt.uuid.to_string(),
+    //                 &rt.liquidation_price.to_string(),
+    //             );
+    //         }
+    //         PositionType::SHORT => {
+    //             redis_db::incrbyfloat(&"TotalShortPositionSize", &rt.positionsize.to_string());
+    //             // trader order set by liquidation_price for short
+    //             redis_db::zadd(
+    //                 &"TraderOrderbyLiquidationPriceFORShort",
+    //                 &rt.uuid.to_string(),
+    //                 &rt.liquidation_price.to_string(),
+    //             );
+    //         }
+    //     }
+
+    //     redis_db::incrbyfloat(&"TotalPoolPositionSize", &rt.positionsize.to_string());
+
+    //     match rt.order_type {
+    //         OrderType::LIMIT => match rt.position_type {
+    //             PositionType::LONG => {
+    //                 redis_db::zadd(
+    //                     &"TraderOrderbyLONGLimit",
+    //                     &rt.uuid.to_string(),
+    //                     &rt.execution_price.to_string(),
+    //                 );
+    //             }
+    //             PositionType::SHORT => {
+    //                 redis_db::zadd(
+    //                     &"TraderOrderbySHORTLimit",
+    //                     &rt.uuid.to_string(),
+    //                     &rt.execution_price.to_string(),
+    //                 );
+    //             }
+    //         },
+    //         _ => {}
+    //     }
+    // });
 }
