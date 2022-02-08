d2ed3861ad0a0ffcbea60e89f7d373c78100d67c
Siddharth Parashar <20308880+shanu516516@users.noreply.github.com>
2022-02-09 03:09:15 +0530

    schedule function added

---
+++ src/relayer/cronjobs.rs
@@ -0,0 +1,48 @@
+use crate::ordertest::generateorder;
+use crate::redislib::redis_db;
+use crate::relayer::*;
+use clokwerk::{Scheduler, TimeUnits};
+use std::time::SystemTime;
+use std::{thread, time};
+
+// use stopwatch::Stopwatch;
+
+pub fn start_cronjobs() {
+    // main thread for scheduler
+    thread::spawn(move || {
+        let mut scheduler = Scheduler::with_tz(chrono::Utc);
+        scheduler.every(2.seconds()).run(move || generateorder());
+
+        // make backup of redis db in backup/redisdb folder every 5 sec //comments for local test
+        scheduler.every(5000.seconds()).run(move || {
+            // scheduler.every(5.seconds()).run(move || {
+            redis_db::save_redis_backup(format!(
+                "backup/redisdb/dump_{}.rdb",
+                SystemTime::now()
+                    .duration_since(SystemTime::UNIX_EPOCH)
+                    .unwrap()
+                    .as_millis()
+            ))
+        });
+
+        // make backup of redis db in backup/redisdb folder every 5 sec //comments for local test
+        scheduler.every(30.seconds()).run(move || {
+            // scheduler.every(1.hour()).run(move || {
+            updatefundingrate(0.001);
+            get_and_update_all_orders_on_funding_cycle();
+        });
+
+        let thread_handle = scheduler.watch_thread(time::Duration::from_millis(100));
+        loop {
+            thread::sleep(time::Duration::from_millis(100000000));
+        }
+    });
+
+    // can't use scheduler because it allows minimum 1 second time to schedule any job
+    thread::spawn(move || loop {
+        thread::sleep(time::Duration::from_millis(250));
+        thread::spawn(move || {
+            getsetlatestprice();
+        });
+    });
+}
