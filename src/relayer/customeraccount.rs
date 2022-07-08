39d41c93f40df1b12aa9ee21f064302ca64e7213
Siddharth Parashar <20308880+shanu516516@users.noreply.github.com>
2022-07-08 14:02:13 +0530

    name change

--- src/relayer/customeraccount.rs
+++ src/relayer/customeraccount.rs
@@ -1,6 +1,6 @@
 use serde_derive::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
-pub struct TraderOrder {
+pub struct AccountId {
     pub pk: String,
     pub auth: String,
     pub comm: String,
