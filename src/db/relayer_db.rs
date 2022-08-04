fe69baf764f3f1dce80fc7412c62d95fd928f1e2
Siddharth Parashar <20308880+shanu516516@users.noreply.github.com>
2022-08-04 20:39:58 +0530

    cmd storage removed

--- src/db/relayer_db.rs
+++ src/db/relayer_db.rs
@@ -34,6 +34,8 @@ pub struct EventLog<T> {
 #[serde(bound(serialize = "T: SerializeAs", deserialize = "T: DeserializeAs<'de>",))]
 pub enum Event<T> {
     TraderOrder(T, RpcCommand, usize),
+    TraderOrderUpdate(T, RelayerCommand, usize),
+    TraderOrderLiquidation(T, RelayerCommand, usize),
     LendOrder(T, RpcCommand, usize),
     RelayerUpdate(T, RelayerCommand, usize),
     Stop(String),
@@ -44,7 +46,6 @@ pub struct OrderDB<T> {
     ordertable: HashMap<Uuid, Arc<RwLock<T>>>,
     sequence: usize,
     nonce: usize,
-    cmd: Vec<RpcCommand>,
     event: Vec<Event<T>>,
     aggrigate_log_sequence: usize,
     last_snapshot_id: usize,
@@ -57,11 +58,12 @@ pub trait LocalDB<T> {
     fn update_nonce(&mut self) -> usize;
     fn get(&mut self, id: Uuid) -> Result<T, std::io::Error>;
     fn get_mut(&mut self, id: Uuid) -> Result<Arc<RwLock<T>>, std::io::Error>;
-    fn update(&mut self, order: T, cmd: RpcCommand) -> Result<T, std::io::Error>;
+    fn update(&mut self, order: T, cmd: RelayerCommand) -> Result<T, std::io::Error>;
     fn remove(&mut self, order: T, cmd: RpcCommand) -> Result<T, std::io::Error>;
     fn aggrigate_log_sequence(&mut self) -> usize;
     fn load_data() -> (bool, OrderDB<T>);
     fn check_backup() -> Self;
+    fn liquidate(&mut self, order: T, cmd: RelayerCommand) -> Result<T, std::io::Error>;
 }
 
 impl LocalDB<TraderOrder> for OrderDB<TraderOrder> {
@@ -70,7 +72,6 @@ impl LocalDB<TraderOrder> for OrderDB<TraderOrder> {
             ordertable: HashMap::new(),
             sequence: 0,
             nonce: 0,
-            cmd: Vec::new(),
             event: Vec::new(),
             aggrigate_log_sequence: 0,
             last_snapshot_id: 0,
@@ -83,7 +84,6 @@ impl LocalDB<TraderOrder> for OrderDB<TraderOrder> {
         order.entry_nonce = get_nonce();
         self.ordertable
             .insert(order.uuid, Arc::new(RwLock::new(order.clone())));
-        self.cmd.push(cmd.clone());
         self.aggrigate_log_sequence += 1;
         self.event.push(Event::<TraderOrder>::new(
             Event::TraderOrder(order.clone(), cmd.clone(), self.aggrigate_log_sequence),
@@ -130,15 +130,14 @@ impl LocalDB<TraderOrder> for OrderDB<TraderOrder> {
 
     fn update(
         &mut self,
-        mut order: TraderOrder,
-        cmd: RpcCommand,
+        order: TraderOrder,
+        cmd: RelayerCommand,
     ) -> Result<TraderOrder, std::io::Error> {
         if self.ordertable.contains_key(&order.uuid) {
             self.ordertable
                 .insert(order.uuid, Arc::new(RwLock::new(order.clone())));
-            self.cmd.push(cmd.clone());
             self.aggrigate_log_sequence += 1;
-            self.event.push(Event::TraderOrder(
+            self.event.push(Event::TraderOrderUpdate(
                 order.clone(),
                 cmd.clone(),
                 self.aggrigate_log_sequence,
@@ -160,11 +159,46 @@ impl LocalDB<TraderOrder> for OrderDB<TraderOrder> {
         if self.ordertable.contains_key(&order.uuid) {
             match self.ordertable.remove(&order.uuid) {
                 Some(_) => {
-                    self.cmd.push(cmd.clone());
                     self.aggrigate_log_sequence += 1;
                     order.exit_nonce = get_nonce();
                     self.event.push(Event::<TraderOrder>::new(
                         Event::TraderOrder(order.clone(), cmd.clone(), self.aggrigate_log_sequence),
+                        String::from("remove_order"),
+                        String::from("TraderOrderEventLog1"),
+                    ));
+                    Ok(order)
+                }
+                None => {
+                    return Err(std::io::Error::new(
+                        std::io::ErrorKind::NotFound,
+                        "Order not found",
+                    ))
+                }
+            }
+        } else {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::NotFound,
+                "Order not found",
+            ));
+        }
+    }
+
+    fn liquidate(
+        &mut self,
+        mut order: TraderOrder,
+        cmd: RelayerCommand,
+    ) -> Result<TraderOrder, std::io::Error> {
+        if self.ordertable.contains_key(&order.uuid) {
+            match self.ordertable.remove(&order.uuid) {
+                Some(_) => {
+                    self.aggrigate_log_sequence += 1;
+                    order.exit_nonce = get_nonce();
+                    self.event.push(Event::<TraderOrder>::new(
+                        Event::TraderOrderLiquidation(
+                            order.clone(),
+                            cmd.clone(),
+                            self.aggrigate_log_sequence,
+                        ),
                         String::from("remove_order"),
                         String::from("TraderOrderEventLog1"),
                     ));
@@ -225,12 +259,37 @@ impl LocalDB<TraderOrder> for OrderDB<TraderOrder> {
             // println!("kafka msg:{:#?}", data.value.clone());
             // if data.key == String::from("StopLoadMSG") {
             match data.value.clone() {
-                Event::TraderOrder(order, cmd, seq) => {
+                Event::TraderOrder(order, _cmd, seq) => {
                     let order_clone = order.clone();
                     database
                         .ordertable
                         .insert(order.uuid, Arc::new(RwLock::new(order)));
-                    database.cmd.push(cmd);
+                    database.event.push(data.value);
+                    if database.sequence < order_clone.entry_sequence {
+                        database.sequence = order_clone.entry_sequence;
+                    }
+                    if database.aggrigate_log_sequence < seq {
+                        database.aggrigate_log_sequence = seq;
+                    }
+                }
+                Event::TraderOrderUpdate(order, _cmd, seq) => {
+                    let order_clone = order.clone();
+                    database
+                        .ordertable
+                        .insert(order.uuid, Arc::new(RwLock::new(order)));
+                    database.event.push(data.value);
+                    if database.sequence < order_clone.entry_sequence {
+                        database.sequence = order_clone.entry_sequence;
+                    }
+                    if database.aggrigate_log_sequence < seq {
+                        database.aggrigate_log_sequence = seq;
+                    }
+                }
+                Event::TraderOrderLiquidation(order, _cmd, seq) => {
+                    let order_clone = order.clone();
+                    if database.ordertable.contains_key(&order.uuid) {
+                        database.ordertable.remove(&order.uuid);
+                    }
                     database.event.push(data.value);
                     if database.sequence < order_clone.entry_sequence {
                         database.sequence = order_clone.entry_sequence;
@@ -279,6 +338,24 @@ impl Event<TraderOrder> {
         match event {
             Event::TraderOrder(order, cmd, seq) => {
                 let event = Event::TraderOrder(order, cmd, seq);
+                let event_clone = event.clone();
+                let pool = KAFKA_EVENT_LOG_THREADPOOL.lock().unwrap();
+                pool.execute(move || {
+                    Event::<TraderOrder>::send_event_to_kafka_queue(event_clone, topic, key);
+                });
+                event
+            }
+            Event::TraderOrderLiquidation(order, cmd, seq) => {
+                let event = Event::TraderOrderLiquidation(order, cmd, seq);
+                let event_clone = event.clone();
+                let pool = KAFKA_EVENT_LOG_THREADPOOL.lock().unwrap();
+                pool.execute(move || {
+                    Event::<TraderOrder>::send_event_to_kafka_queue(event_clone, topic, key);
+                });
+                event
+            }
+            Event::TraderOrderUpdate(order, cmd, seq) => {
+                let event = Event::TraderOrderUpdate(order, cmd, seq);
                 let event_clone = event.clone();
                 let pool = KAFKA_EVENT_LOG_THREADPOOL.lock().unwrap();
                 pool.execute(move || {
@@ -364,7 +441,6 @@ impl LocalDB<LendOrder> for OrderDB<LendOrder> {
             ordertable: HashMap::new(),
             sequence: 0,
             nonce: 0,
-            cmd: Vec::new(),
             event: Vec::new(),
             aggrigate_log_sequence: 0,
             last_snapshot_id: 0,
@@ -372,14 +448,13 @@ impl LocalDB<LendOrder> for OrderDB<LendOrder> {
     }
 
     fn add(&mut self, mut order: LendOrder, cmd: RpcCommand) -> LendOrder {
-        let mut lendpool = LEND_POOL_DB.lock().unwrap();
+        // let mut lendpool = LEND_POOL_DB.lock().unwrap();
         // lendpool.sequence;
         self.sequence += 1;
         order.entry_sequence = self.sequence;
-        order.entry_nonce = self.nonce;
+        order.entry_nonce = get_nonce();
         self.ordertable
             .insert(order.uuid, Arc::new(RwLock::new(order.clone())));
-        self.cmd.push(cmd.clone());
         self.aggrigate_log_sequence += 1;
         self.event.push(Event::<LendOrder>::new(
             Event::LendOrder(order.clone(), cmd.clone(), self.aggrigate_log_sequence),
@@ -426,17 +501,8 @@ impl LocalDB<LendOrder> for OrderDB<LendOrder> {
     fn update(
         &mut self,
         mut order: LendOrder,
-        cmd: RpcCommand,
+        cmd: RelayerCommand,
     ) -> Result<LendOrder, std::io::Error> {
-        self.ordertable
-            .insert(order.uuid, Arc::new(RwLock::new(order.clone())));
-        self.cmd.push(cmd.clone());
-        self.aggrigate_log_sequence += 1;
-        self.event.push(Event::TraderOrder(
-            order.clone(),
-            cmd.clone(),
-            self.aggrigate_log_sequence,
-        ));
         Ok(order.clone())
     }
 
@@ -447,10 +513,9 @@ impl LocalDB<LendOrder> for OrderDB<LendOrder> {
     ) -> Result<LendOrder, std::io::Error> {
         match self.ordertable.remove(&order.uuid) {
             Some(_) => {
-                self.cmd.push(cmd.clone());
                 self.aggrigate_log_sequence += 1;
                 order.exit_nonce = get_nonce();
-                self.event.push(Event::TraderOrder(
+                self.event.push(Event::LendOrder(
                     order.clone(),
                     cmd.clone(),
                     self.aggrigate_log_sequence,
@@ -465,7 +530,13 @@ impl LocalDB<LendOrder> for OrderDB<LendOrder> {
             }
         }
     }
-
+    fn liquidate(
+        &mut self,
+        order: LendOrder,
+        cmd: RelayerCommand,
+    ) -> Result<LendOrder, std::io::Error> {
+        Ok(order)
+    }
     fn aggrigate_log_sequence(&mut self) -> usize {
         self.aggrigate_log_sequence
     }
@@ -499,12 +570,11 @@ impl LocalDB<LendOrder> for OrderDB<LendOrder> {
         while stop_signal {
             let data = recever1.recv().unwrap();
             match data.value.clone() {
-                Event::LendOrder(order, cmd, seq) => {
+                Event::LendOrder(order, _cmd, seq) => {
                     let order_clone = order.clone();
                     database
                         .ordertable
                         .insert(order.uuid, Arc::new(RwLock::new(order)));
-                    database.cmd.push(cmd);
                     database.event.push(data.value);
                     if database.sequence < order_clone.entry_sequence {
                         database.sequence = order_clone.entry_sequence;
@@ -521,7 +591,13 @@ impl LocalDB<LendOrder> for OrderDB<LendOrder> {
                 Event::TraderOrder { .. } => {
                     println!("TraderOrder Im here");
                 }
+                Event::TraderOrderUpdate { .. } => {
+                    println!("TraderOrder Im here");
+                }
                 Event::RelayerUpdate { .. } => {
+                    println!("RelayerUpdate Im here");
+                }
+                Event::TraderOrderLiquidation { .. } => {
                     println!("RelayerUpdate Im here");
                 }
             }
@@ -551,6 +627,10 @@ impl Event<LendOrder> {
     pub fn new(event: Event<LendOrder>, key: String) -> Self {
         match event {
             Event::TraderOrder(order, cmd, seq) => Event::TraderOrder(order, cmd, seq),
+            Event::TraderOrderLiquidation(order, cmd, seq) => {
+                Event::TraderOrderLiquidation(order, cmd, seq)
+            }
+            Event::TraderOrderUpdate(order, cmd, seq) => Event::TraderOrderUpdate(order, cmd, seq),
             Event::LendOrder(order, cmd, seq) => {
                 let event = Event::LendOrder(order, cmd, seq);
                 let event_clone = event.clone();
@@ -636,11 +716,10 @@ impl Event<LendOrder> {
 }
 
 pub fn get_nonce() -> usize {
-    let nonce = GLOBAL_NONCE.read().unwrap();
-    *nonce
+    let mut lend_pool = LEND_POOL_DB.lock().unwrap();
+    lend_pool.get_nonce()
 }
 pub fn update_nonce() -> usize {
-    let mut nonce = GLOBAL_NONCE.write().unwrap();
-    *nonce += 1;
-    *nonce
+    let mut lend_pool = LEND_POOL_DB.lock().unwrap();
+    lend_pool.next_nonce()
 }
